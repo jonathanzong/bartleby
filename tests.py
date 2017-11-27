8f6a773adce04e5734f55282bbace6c743623233
Jonathan Zong <jonathan@jonathanzong.com>
2017-11-27 02:01:06 -0500

    place to put unit tests

---
+++ tests.py
@@ -0,0 +1,19 @@
+import os
+import main
+import unittest
+import tempfile
+
+class FlaskrTestCase(unittest.TestCase):
+
+  def setUp(self):
+    pass
+
+  def tearDown(self):
+    pass
+
+  # TODO: write tests!!!
+  def test_noop(self):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
