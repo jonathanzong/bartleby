e5f0b2c7b670d23aeb403d35db2841e93bd8198f
Nathan Matias <natematias@gmail.com>
2017-11-29 18:29:04 +0530

    solved alembic multiple database issue and pushed first user table

--- app/models.py
+++ app/models.py
@@ -15,12 +15,9 @@ Base = declarative_base()
 
 class TwitterUser(Base):
     __tablename__ = 'twitter_users'
-    id                  = Column(String(64), primary_key = True) # should be lowercase; if not found, # if not found, NOT_FOUND_TWITTER_USER_STR_[date]
-    not_found_id        = Column(String(64), index = True, default=None)    # if a user ever goes between e.g. FOUND and NOT_FOUND (either direction), 
-                                                                            # we want to be able to map between the actual id and the <NOT_FOUND>... id 
+    id                  = Column(String(64), primary_key = True) # should be lowercase
     screen_name         = Column(String(256), index = True) # if not found, # if not found, NOT_FOUND_TWITTER_USER_STR
     created_at          = Column(DateTime)
-    record_created_at   = Column(DateTime, default=datetime.datetime.utcnow)
+    account_created_at  = Column(DateTime, default=datetime.datetime.utcnow)
     lang                = Column(String(32))
     user_state          = Column(Integer) # utils/common.py
-    CS_oldest_tweets_archived = Column(Integer, default=1) # see CS_JobState Enum
