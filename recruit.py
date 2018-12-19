42f23310ba62638ab7deeb5e168062ba75ea04ad
Jonathan Zong <jonathan.zong@gmail.com>
2018-12-19 17:49:48 -0600

    fix missing var in recruit

--- recruit.py
+++ recruit.py
@@ -6,8 +6,11 @@ BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 CONFIG_DIR = os.path.join(BASE_DIR, "config")
 db_session = DbEngine(CONFIG_DIR + "/{env}.json".format(env=ENV)).new_session()
 
+DEFAULT_STUDY = 'academic'  # set default template for direct link to homepage here
+
 sce = TwitterDebriefExperimentController(
     experiment_name='twitter_academic_debrief_experiment',
+    default_study=DEFAULT_STUDY,
     db_session=db_session,
     required_keys=['name', 'randomizations', 'eligible_ids']
   )
