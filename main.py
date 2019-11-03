60620bcd67b5fce0a888ecd11edd3ed92643de6f
Jonathan Zong <jonathan.zong@gmail.com>
2019-11-03 00:49:35 -0400

    reddit authorization flow

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import twitter_api_keys
+import reddit_api_keys
 import pybrake_key
 import tweepy
 import json
 import os
 import datetime
+import praw
+import uuid
 
 from flask import Flask, session, request, redirect, url_for, render_template
 
@@ -18,7 +21,7 @@ from app.controllers.twitter_debrief_experiment_controller import *
 app = Flask(__name__)
 app.secret_key = 'such secret very key!' # session key
 app.config['PYBRAKE'] = dict(
-    host='https://dmca-debrief-errbit.herokuapp.com',
+    host=pybrake_key.PYBRAKE_HOST,
     project_id=1212,
     project_key=pybrake_key.PYBRAKE_KEY,
 )
@@ -55,7 +58,7 @@ def index():
 
   study_template = request.args.get('t') if request.args.get('t') else DEFAULT_STUDY
   extra_data = json.loads(request.args.get('x')) if request.args.get('x') else None
-  return render_template(study_template + '/01-index.html', amount_dollars=amount_dollars, extra_data=extra_data)
+  return render_template(study_template + '/index.html', amount_dollars=amount_dollars, extra_data=extra_data)
 
 @app.route('/debrief', methods=('GET', 'POST'))
 def debrief():
@@ -99,7 +102,7 @@ def debrief():
 
     # return redirect(url_for('complete'))
   study_template = sce.get_user_study_template(user)
-  return render_template(study_template + '/05-debrief.html', user=user, form=form)
+  return render_template(study_template + '/debrief.html', user=user, form=form)
 
 @app.route('/ineligible')
 def ineligible():
@@ -129,6 +132,25 @@ def login():
     return redirect(redirect_url)
   except tweepy.TweepError:
     return 'Error! Failed to get request token.'
+
+@app.route('/login_reddit')
+def login_reddit():
+  if is_logged_in():
+    return redirect(url_for('debrief'))
+
+  sce.record_user_action(None, 'login_attempt', None)
+
+  redirect_uri = url_for('oauth_authorized_reddit', _external=True)
+  reddit = praw.Reddit(client_id=reddit_api_keys.REDDIT_CLIENT_ID,
+                     client_secret=reddit_api_keys.REDDIT_CLIENT_SECRET,
+                     redirect_uri=redirect_uri,
+                     user_agent='debriefing.media.mit.edu')
+
+  state_string = str(uuid.uuid4())
+  session['state_string'] = state_string
+
+  auth_url = reddit.auth.url(['identity'], state_string, 'permanent')
+  return redirect(auth_url)
 
 @app.route('/logout')
 def logout():
@@ -172,7 +194,7 @@ def oauth_authorized():
       return redirect(url_for('ineligible'))
 
     study_data = sce.get_user_study_data(user)
-    if study_data is not None:
+    if study_data is not None: # TODO: this needs to be generalized somehow to be configurable per study
       study_data["account_created_at"] = study_data["created_at"]
       del study_data["created_at"]
       study_data["account_created_at"] = datetime.datetime.strptime(study_data["account_created_at"], "%Y-%m-%d %H:%M:%S")
@@ -194,6 +216,74 @@ def oauth_authorized():
   except tweepy.TweepError:
     # return 'Error! Failed to get access token.'
     return redirect(url_for('index'))
+
+@app.route('/oauth_authorized_reddit')
+def oauth_authorized_reddit():
+  print(session)
+  if 'state_string' not in session:
+    return redirect(url_for('index'))
+
+  error = request.args.get('error')
+  print(error)
+  if error == 'access_denied':
+    return redirect(url_for('index'))
+
+  state = request.args.get('state')
+  print(state)
+  sent_state = session['state_string']
+  del session['state_string']
+  if not sent_state == state:
+    return redirect(url_for('index'))
+
+  redirect_uri = url_for('oauth_authorized_reddit', _external=True)
+  reddit = praw.Reddit(client_id=reddit_api_keys.REDDIT_CLIENT_ID,
+                     client_secret=reddit_api_keys.REDDIT_CLIENT_SECRET,
+                     redirect_uri=redirect_uri,
+                     user_agent='debriefing.media.mit.edu')
+
+  authorization_code = request.args.get('code')
+  refresh_token = reddit.auth.authorize(authorization_code)
+
+  user = reddit.user.me() # PRAW Redditor instance
+
+  today = datetime.datetime.now()
+  created = datetime.datetime.fromtimestamp(user.created_utc)
+  account_age = today.year - created.year - ((today.month, today.day) < (created.month, created.day))
+
+  session['user'] = {
+    'id': user.id,
+    'screen_name': user.name,
+    'created_at': created.isoformat(),
+    'account_age': account_age
+  }
+  user = session['user']
+
+  print(user)
+
+  if not sce.is_eligible(user):
+    return redirect(url_for('ineligible'))
+
+  study_data = sce.get_user_study_data(user)
+  if study_data is not None:
+    pass
+    # study_data["account_created_at"] = study_data["created_at"]
+#     del study_data["created_at"]
+#     study_data["account_created_at"] = datetime.datetime.strptime(study_data["account_created_at"], "%Y-%m-%d %H:%M:%S")
+#     study_data["notice_date"] = datetime.datetime.strptime(study_data["notice_date"], "%Y-%m-%d %H:%M:%S")
+#     study_data["start_date"] = study_data["notice_date"] - datetime.timedelta(days=23)
+#     study_data["end_date"] = study_data["notice_date"] + datetime.timedelta(days=23)
+#     study_data["account_created_at"] = study_data["account_created_at"].strftime("%B %-d, %Y")
+#     study_data["notice_date"] = study_data["notice_date"].strftime("%B %-d, %Y")
+#     study_data["start_date"] = study_data["start_date"].strftime("%B %-d, %Y")
+#     study_data["end_date"] = study_data["end_date"].strftime("%B %-d, %Y")
+#     session['user'].update(study_data)
+
+  # create user if not exists
+  sce.create_user_if_not_exists(user)
+
+  sce.record_user_action(user, 'login_success', None)
+
+  return redirect(url_for('debrief'))
 
 @app.errorhandler(404)
 def page_not_found(e):
