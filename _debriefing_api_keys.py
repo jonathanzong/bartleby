192139f53ec6a08cab356c41dd948becb87528f3
Jonathan Zong <jzong@mit.edu>
2020-06-06 19:46:53 -0400

    api keys example file

---
+++ _debriefing_api_keys.py
@@ -0,0 +1,21 @@
+# reddit api keys
+REDDIT_CLIENT_ID=""
+REDDIT_CLIENT_SECRET=""
+
+# twitter api keys
+TWITTER_CONSUMER_KEY=""
+TWITTER_CONSUMER_SECRET=""
+
+# pybrake error monitoring keys
+PYBRAKE_HOST=""
+PYBRAKE_KEY=""
+
+# twitter sender api keys
+TWITTER_SENDER_CONSUMER_KEY=""
+TWITTER_SENDER_CONSUMER_SECRET=""
+TWITTER_SENDER_ACCESS_TOKEN=""
+TWITTER_SENDER_ACCESS_TOKEN_SECRET=""
+
+# paypal api sender keys
+PAYPAL_CLIENT_ID = ""
+PAYPAL_CLIENT_SECRET = ""
