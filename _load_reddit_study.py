020e64bbc512ab904aee778845eaf356d7f4cc4b
Jonathan Zong <jzong@mit.edu>
2020-06-10 14:44:21 -0400

    templ

--- _load_reddit_study.py
+++ _load_reddit_study.py
@@ -11,9 +11,12 @@ db_session = DbEngine(CONFIG_DIR + "/{env}.json".format(env=ENV)).new_session()
 
 ## create experiment record
 
+experiment_name = ''
+
 experiment = Experiment(
   url_id='',
-  experiment_name='',
+  experiment_name=experiment_name,
+  platform='reddit',
   study_template='')
 db_session.add(experiment)
 
@@ -24,7 +27,9 @@ data = {}
 with open('r-feminism-study-data-merged-05.12.2020.csv') as datafile:
   reader = csv.DictReader(datafile)
   for row in reader:
-    data[row["id"]] = json.dumps(row)
+    uid = row['id']
+    del row['id']
+    data[uid] = json.dumps(row)
 
 with open('r-feminism-account-mapping-05.12.2020.csv') as mappingfile:
   reader = csv.DictReader(mappingfile)
@@ -32,7 +37,6 @@ with open('r-feminism-account-mapping-05.12.2020.csv') as mappingfile:
     name = row['username'].casefold()
     uid = row['uuid']
     study_data_json = data[uid]
-    del study_data_json['id']
     maybe_participant_eligibility = db_session.query(ParticipantEligibility).filter_by(experiment_name=experiment_name, participant_user_id=name).first()
     if not maybe_participant_eligibility:
       participant_eligibility = ParticipantEligibility(experiment_name=experiment_name, participant_user_id = name, study_data_json = study_data_json.encode('utf-8'))
